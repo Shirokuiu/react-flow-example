50355917877414ea554bd18632a8dd01c5f5ce36
pavel <shirokuiuna@mail.ru>
2023-01-31 21:12:33 +0300

    Рефакторинг экспорта экшенов из слайса редакса

--- src/components/pages/redux-example-page/redux-example-page.tsx
+++ src/components/pages/redux-example-page/redux-example-page.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import CenterLayout from 'src/components/layouts/center-layout/center-layout';
 import PageLayout from 'src/components/layouts/page-layout/page-layout';
 import { useAppDispatch, useAppSelector } from 'src/hooks';
-import { clearState } from 'src/store/slices/main-page-slice/main-page-slice';
+import { mainPageSliceActions } from 'src/store/slices/main-page-slice/main-page-slice';
 import { getTitle } from 'src/store/slices/main-page-slice/selectors';
 
 function ReduxExamplePage() {
@@ -11,7 +11,7 @@ function ReduxExamplePage() {
   const dispatch = useAppDispatch();
 
   const handleButtonClick = () => {
-    dispatch(clearState());
+    dispatch(mainPageSliceActions.clearState());
   };
 
   return (
